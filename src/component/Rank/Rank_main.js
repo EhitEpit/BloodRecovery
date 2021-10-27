dee5d9f632a5b84d09b8eea24ada36aa329d5d88
gobong <chy9386@gmail.com>
2021-10-27 17:50:15 +0900

    현영 2021-10-27 5:49

--- src/component/Rank/Rank_main.js
+++ src/component/Rank/Rank_main.js
@@ -1,28 +1,28 @@
 import React from "react";
 import Menu_nav_text from "../Common/Menu_nav_text";
 import axios from "axios";
+import "./Rank_main.css";
 function Rank_main() {
-  axios({
-    method: "GET",
-    url: "/rankings",
-    // data: {
-    //   //   id: id.value,
-    //   //   userId: userId.value,
-    //   //   userNickname: userNickname.value,
-    //   //   userProfile: userProfile.value,
-    //   //   userPoint: userPoint.value,
-    // },
-  })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  // axios({
+  //   method: "GET",
+  //   url: "http://localhost:8080"
+  // })
+  //   .then((res) => {
+  //     console.log(res);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 
   return (
     <div>
-      <Menu_nav_text name={"랭킹"}></Menu_nav_text>
+      <div className="title">
+        <Menu_nav_text name={"(아이콘)랭킹"}></Menu_nav_text>
+      </div>
+      <div className="top">
+        "최고봉(닉네임) 기부천사(등급)님의 포인트는 1000P(현재 포인트)입니다.
+        현재 랭킹은 1(순위)위 입니다!"
+      </div>
     </div>
   );
 }
